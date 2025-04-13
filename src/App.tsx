a6251b64ea3b63c8607158cba75fff90b8893ced
Junaidfaiz1 <he630001@gmail.com>
2025-04-13 10:46:39 -0700

    Update dependencies for react-router and react-router-dom to version 7.5.0; refactor App component to implement nested routing for dashboard features including issued books, overdue books, and user management.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import UserAndBookLists from "./Pages/UserAndBookLists";
 import IssuedBookDashboard from "./components/IssuedBookDashboard";
 import { ToastContainer } from "react-toastify";
 import { Navbar } from "./components/Navbar";
-function App() {
+import OverdueBookLists from "./components/OverdueBookLists";
+import DashboardUser from "./components/DashboardUsers";
+
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+const Home: React.FC = () => {
   return (
     <div className="dark:bg-stone-950 dark:text-white bg-gray-100 text-black">
       <ToastContainer />
@@ -16,6 +27,33 @@ function App() {
       <IssuedBookDashboard />
     </div>
   );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/dashboard"
+        element={
+          <>
+            <Navbar />
+            <div className="mt-4">
+              <Outlet /> {/* Add this to render nested routes */}
+            </div>
+          </>
+        }
+      >
+        <Route path="issuedbook" element={<IssuedBookDashboard />} />
+        <Route path="overduebook" element={<OverdueBookLists />} />
+        <Route path="user" element={<DashboardUser />} />
+      </Route>
+    </>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
